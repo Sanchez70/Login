f392e2ab4e33605121583e1b84ecd8bc848dd340
Sanchez70 <124312881+Sanchez70@users.noreply.github.com>
2023-04-27 16:25:44 -0500

    login30

--- Login/src/login/Logiin1.java
+++ Login/src/login/Logiin1.java
@@ -27,6 +27,7 @@ public class Logiin1 extends javax.swing.JFrame {
     public Logiin1() {
         initComponents();
         this.setLocationRelativeTo(null);
+       
     }
 
     public void iniciar() throws SQLException {
@@ -38,18 +39,7 @@ public class Logiin1 extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "CONTRASEÑA O USUARIO INCORRECTO");
         }
     }
-    public void restablecer() throws SQLException{
-         String sql = "SELECT  usuario FROM public.usuarios where usuario='" + usu1.getText()+"'";
-        ResultSet contenedor = conexion.Consulta(sql);
-        if (contenedor.next()) {
-            usuario nuevo1= new usuario();
-            nuevo1.setUsuario(usu1.getText());
-            nuevo1.setContra(contra1.getText().toString());
-            nuevo1.Modificar();
-        } else {
-            JOptionPane.showMessageDialog(this, "USUARIO NO ENCONTRADO");
-        }
-    }
+
 
     
 
@@ -63,114 +53,109 @@ public class Logiin1 extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        usu1 = new javax.swing.JTextField();
-        jLabel10 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
-        jLabel11 = new javax.swing.JLabel();
-        contra1 = new javax.swing.JPasswordField();
+        fSLabel1 = new LIB.FSLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        usu = new javax.swing.JTextField();
-        contra = new javax.swing.JPasswordField();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        contra = new org.edisoncor.gui.passwordField.PasswordFieldRoundBackground();
+        usu = new org.edisoncor.gui.textField.TextFieldRoundBackground();
+        fSButtonMD1 = new LIB.FSButtonMD();
+        panelCurves1 = new org.edisoncor.gui.panel.PanelCurves();
+        fSButtonMD2 = new LIB.FSButtonMD();
+        jSeparator1 = new javax.swing.JSeparator();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(0, 255, 153));
+        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
         jPanel1.setBorder(javax.swing.BorderFactory.createCompoundBorder());
         jPanel1.setForeground(new java.awt.Color(153, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel7.setText("RESTABLECER CONTRASEÑA");
-        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 30, 152, 25));
-
-        jLabel8.setText("Usuario:");
-        jPanel2.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 90, -1, -1));
-        jPanel2.add(usu1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 89, 160, 20));
-
-        jLabel10.setText("contraseña:");
-        jPanel2.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 130, -1, -1));
-
-        jLabel9.setText("Guardar");
-        jLabel9.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabel9MouseClicked(evt);
-            }
-        });
-        jPanel2.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 180, -1, -1));
-
-        jLabel11.setText("Volver");
-        jPanel2.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 180, -1, -1));
-
-        contra1.setText("jPasswordField1");
-        jPanel2.add(contra1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 129, 160, 20));
-
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 140, 580, 310));
+        fSLabel1.setBackground(new java.awt.Color(204, 204, 204));
+        fSLabel1.setForeground(new java.awt.Color(204, 204, 204));
+        fSLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/login/edificio.jpg"))); // NOI18N
+        fSLabel1.setText("");
+        fSLabel1.setToolTipText("");
+        fSLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        fSLabel1.setLineColor(new java.awt.Color(204, 204, 204));
+        jPanel1.add(fSLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 160, 530, 450));
 
         jLabel1.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(51, 51, 51));
         jLabel1.setText("Usuario:");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 180, 90, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 90, -1));
 
         jLabel2.setFont(new java.awt.Font("Stencil", 0, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(51, 51, 51));
         jLabel2.setText("Contraseña:");
         jLabel2.setDoubleBuffered(true);
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 260, -1, -1));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 310, -1, -1));
 
-        usu.setBackground(new java.awt.Color(102, 255, 255));
-        usu.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/login/logo desenfocado4.png"))); // NOI18N
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(-50, 0, 960, -1));
+
+        jLabel5.setFont(new java.awt.Font("SimSun", 0, 10)); // NOI18N
+        jLabel5.setText("Restablacer Contraseña");
+        jLabel5.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        jLabel5.setNextFocusableComponent(fSButtonMD1);
+        jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel5MouseClicked(evt);
+            }
+        });
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 390, -1, 20));
+
+        contra.setText("passwordFieldRoundBackground1");
+        contra.setFont(new java.awt.Font("Arial", 0, 10)); // NOI18N
+        contra.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                contraActionPerformed(evt);
+            }
+        });
+        jPanel1.add(contra, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 300, 190, 30));
+
+        usu.setFont(new java.awt.Font("Arial Nova Light", 0, 14)); // NOI18N
         usu.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 usuActionPerformed(evt);
             }
         });
-        jPanel1.add(usu, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 210, 190, 30));
+        jPanel1.add(usu, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 230, 190, 30));
 
-        contra.setBackground(new java.awt.Color(102, 255, 255));
-        contra.setText("jPasswordField1");
-        contra.setBorder(null);
-        contra.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                contraMouseClicked(evt);
+        fSButtonMD1.setBackground(new java.awt.Color(0, 204, 204));
+        fSButtonMD1.setText("REGISTRARSE");
+        fSButtonMD1.setColorNormal(new java.awt.Color(0, 204, 204));
+        fSButtonMD1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fSButtonMD1ActionPerformed(evt);
             }
         });
-        jPanel1.add(contra, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 300, 190, 30));
+        jPanel1.add(fSButtonMD1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 350, 150, 30));
+        jPanel1.add(panelCurves1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 850, 160));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/login/logo desenfocado4.png"))); // NOI18N
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(-40, 0, 960, -1));
+        fSButtonMD2.setBackground(new java.awt.Color(0, 204, 204));
+        fSButtonMD2.setText("INICIAR SESIÓN");
+        fSButtonMD2.setColorNormal(new java.awt.Color(0, 204, 204));
+        fSButtonMD2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fSButtonMD2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(fSButtonMD2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 350, 150, 30));
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 2, 10)); // NOI18N
-        jLabel4.setText("REGISTRARSE");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 370, -1, 10));
-
-        jLabel6.setBackground(new java.awt.Color(153, 255, 255));
-        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel6.setText("INICIAR SESIÓN");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 340, -1, -1));
-
-        jLabel5.setText("Restablacer Contraseña");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 390, -1, -1));
+        jSeparator1.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 410, 170, 20));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 842, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 510, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 552, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -180,17 +165,29 @@ public class Logiin1 extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_usuActionPerformed
 
-    private void contraMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_contraMouseClicked
-        contra.setText("");
-    }//GEN-LAST:event_contraMouseClicked
+    private void contraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_contraActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_contraActionPerformed
 
-    private void jLabel9MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel9MouseClicked
+    private void fSButtonMD2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fSButtonMD2ActionPerformed
         try {
-            restablecer();
+            iniciar();
         } catch (SQLException ex) {
             Logger.getLogger(Logiin1.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_jLabel9MouseClicked
+    }//GEN-LAST:event_fSButtonMD2ActionPerformed
+
+    private void fSButtonMD1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fSButtonMD1ActionPerformed
+       Registrar nuevo= new Registrar();
+       nuevo.setVisible(true);
+       dispose();
+    }//GEN-LAST:event_fSButtonMD1ActionPerformed
+
+    private void jLabel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseClicked
+       contraseña nueva = new contraseña();
+       nueva.setVisible(true);
+       dispose();
+    }//GEN-LAST:event_jLabel5MouseClicked
 
     /**
      * @param args the command line arguments
@@ -228,22 +225,17 @@ public class Logiin1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPasswordField contra;
-    private javax.swing.JPasswordField contra1;
+    private org.edisoncor.gui.passwordField.PasswordFieldRoundBackground contra;
+    private LIB.FSButtonMD fSButtonMD1;
+    private LIB.FSButtonMD fSButtonMD2;
+    private LIB.FSLabel fSLabel1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JTextField usu;
-    private javax.swing.JTextField usu1;
+    private javax.swing.JSeparator jSeparator1;
+    private org.edisoncor.gui.panel.PanelCurves panelCurves1;
+    private org.edisoncor.gui.textField.TextFieldRoundBackground usu;
     // End of variables declaration//GEN-END:variables
 }
